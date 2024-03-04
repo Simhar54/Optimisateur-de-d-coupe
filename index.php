1ac131ecb73adfc50518853b04184d00310d61be
Arnaud Simoncelli <a.simoncelli54@gmail.com>
2024-03-04 10:34:46 +0100

    create base files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Optimisateur de découpe</title>
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/CSS/bootstrap.min.css">
+</head>
+<body>
+    
+
+    <script src="assets/JS/mainscript.js"></script>
+    <script src="assets/JS/bootstrap.bundle.min.js"></script>
+</body>
+</html>

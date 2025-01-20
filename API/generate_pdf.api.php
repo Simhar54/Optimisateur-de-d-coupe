77bc258eb71fdc2155a28e6639e5f79fc7b3785c
Arnaud Simoncelli <a.simoncelli54@gmail.com>
2025-01-20 16:32:12 +0100

    Fix bug pdf generate

--- API/generate_pdf.api.php
+++ API/generate_pdf.api.php
@@ -19,21 +19,35 @@ libxml_use_internal_errors(true);
 $dom = new DOMDocument('1.0', 'UTF-8'); // Spécifie l'encodage en UTF-8
 $dom->loadHTML(mb_convert_encoding($tableHtml, 'HTML-ENTITIES', 'UTF-8'));
 
-// Utilisation de DOMXPath pour sélectionner les lignes sans "OF" dans la deuxième cellule
+// Modification de la requête XPath pour chercher les lignes sans "WO" ou "OF"
 $xpath = new DOMXPath($dom);
-$rows = $xpath->query("//tr[td[2][not(contains(., 'OF'))]]");
+$rows = $xpath->query("//tr[td[2][not(contains(., 'WO')) and not(contains(., 'OF'))]]");
 
 // Suppression des lignes sélectionnées
 foreach ($rows as $row) {
     $row->parentNode->removeChild($row);
 }
 
+// Nettoyage des attributs data-i18n pour le PDF
+$elements = $xpath->query("//*[@data-i18n]");
+foreach ($elements as $element) {
+    if ($element instanceof DOMElement) {
+        $element->removeAttribute('data-i18n');
+    }
+}
+
+// Suppression des spans vides qui contenaient les traductions
+$spans = $xpath->query("//span[not(text())]");
+foreach ($spans as $span) {
+    $span->parentNode->removeChild($span);
+}
+
 // Sauvegarde du HTML filtré
 $filteredHtml = $dom->saveHTML();
 
-// Ajout de bordures solides aux cellules de la table
-$filteredHtml = str_replace("<td", "<td style='border: 1px solid black;'", $filteredHtml);
-$filteredHtml = str_replace("<th", "<th style='border: 1px solid black;'", $filteredHtml);
+// Style des cellules
+$filteredHtml = str_replace("<td", "<td style='border: 1px solid black; padding: 5px;'", $filteredHtml);
+$filteredHtml = str_replace("<th", "<th style='border: 1px solid black; padding: 5px;'", $filteredHtml);
 
 // Ajout de la date et l'heure au titre du PDF
 $date = date("d-m-Y H:i:s");
